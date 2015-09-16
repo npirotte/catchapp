b491210f8317f95d38507e733d5a86f95c1737ad
Benjamin Pirotte <benjamin.divvision@gmail.com>
2015-09-16 20:42:17 +0200

    avatar js

--- src/js/components/ItemAvatar.js
+++ src/js/components/ItemAvatar.js
@@ -1,6 +1,6 @@
 var React = require('react');
 
-const AVATAR_CLASSNAME = 'ListItem__avatar';
+const AVATAR_CLASSNAME = 'item-avatar';
 
 function _getInitial(input)
 {
@@ -11,15 +11,22 @@ module.exports = React.createClass({
 
 	render ()
 	{
-		var className = AVATAR_CLASSNAME;
+		if( this.props.losange ){
+			return this.props.src ? this.renderLosangeImage() : this.renderLosangeInitial();
+		}else{
+			return this.props.src ? this.renderImage() : this.renderInitial();
+		}
 
-		return this.props.src ? this.renderImage() : this.renderInitial();
 	},
 
 	renderImage()
 	{
 		var className = AVATAR_CLASSNAME;
-		className += ' ListItem__avatar--image ' + this.props.className;
+		className +=  ' ' + AVATAR_CLASSNAME + '--image' ;
+
+		if(this.props.className){
+			className += ' ' + this.props.className
+		}
 
 		var style = { backgroundImage : 'url(' + this.props.src + ')' }
 
@@ -30,14 +37,64 @@ module.exports = React.createClass({
 
 	renderInitial()
 	{
-		var className = AVATAR_CLASSNAME;
 		var initial = _getInitial(this.props.name);
 
-		className += ' ListItem__avatar--initial ListItem__avatar--initial--' + initial.toLowerCase();
+		var className = AVATAR_CLASSNAME;
+		className += ' ' + AVATAR_CLASSNAME + '--initial' + AVATAR_CLASSNAME + '--initial--' + initial.toLowerCase();
+		
+		if(this.props.className){
+			className += ' ' + this.props.className
+		}
 
 		return (
 			<span className={className} >{initial}</span>
 			);
+	},
+
+	renderLosangeImage()
+	{
+
+		var className = AVATAR_CLASSNAME;
+		className +=  ' ' + AVATAR_CLASSNAME + '--image_losange' ;
+
+		if(this.props.className){
+			className += ' ' + this.props.className
+		}
+
+		if( this.props.color ){
+			className +=  ' ' + AVATAR_CLASSNAME + '--image_losange_' + this.props.color;
+		}
+
+		var src = this.props.src;
+
+		return (
+				<div alt={this.props.name} className={className}>
+					<div style={{backgroundImage : 'url(' +src + ')'}}></div>
+				</div>
+			);
+	},
+
+	renderLosangeInitial()
+	{
+		var initial = _getInitial(this.props.name);
+
+		var className = AVATAR_CLASSNAME;
+		className +=  ' ' + AVATAR_CLASSNAME + '--initial_losange' ;
+
+		if(this.props.className){
+			className += ' ' + this.props.className
+		}
+
+		if( this.props.color ){
+			className +=  ' ' + AVATAR_CLASSNAME + '--initial_losange_' + this.props.color;
+		}
+
+		return (
+				<div alt={this.props.name} className={className}>
+					<div>{initial}</div>
+				</div>
+			);
 	}
+
 
 });
